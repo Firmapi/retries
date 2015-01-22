df3c6ecb33ee3ad74a3e8cada53f73a8d9e3a2c5
Antoine Finkelstein <antoine.fink@gmail.com>
2015-01-22 16:40:53 +0100

    Massive refacotring and simplification

--- lib/retries.rb
+++ lib/retries.rb
@@ -3,39 +3,25 @@ module Kernel
   # backoff.
   #
   # max_tries - The maximum number of times to runthe block.
-  # base_sleep_seconds - The starting delay between retries.
-  # max_sleep_seconds - The maximum retries delay.
+  # delay - The delay between retries.
   # handler - If not `nil`, a `Proc` that will be called for each retry. It will
-  #           be passed three arguments, `exception` (the rescued exception),
-  #           `attempt_number`, and `total_delay` (seconds since start of first
-  #           attempt).
+  #           be passed two arguments, `exception` (the rescued exception) and
+  #           `attempt_number`.
   # recover - A specific exception class to recover or an array of classes.
   #
-  def with_retries(max_tries: 3, base_sleep_seconds: 0.5, max_sleep_seconds: 1.0, handler: nil, recover: nil, &block)
-    fail "max_tries must be greater than 0." unless max_tries > 0
-    if base_sleep_seconds > max_sleep_seconds
-      fail "base_sleep_seconds cannot be greater than :max_sleep_seconds."
-    end
-
-    exception_types_to_rescue = Array(recover || StandardError)
-    fail "with_retries must be passed a block" unless block_given?
-
+  def with_retries(max_tries: 3, delay: 0.8, handler: nil, recover: nil, &block)
     attempts = 0
-    start_time = Time.now
+
     begin
       attempts += 1
       return block.call(attempts)
-    rescue *exception_types_to_rescue => exception
+
+    rescue *Array(recover || StandardError) => exception
       raise exception if attempts >= max_tries
-      handler.call(exception, attempts, Time.now - start_time) if handler
-      # The sleep time is an exponentially-increasing function of base_sleep_seconds. But, it never exceeds
-      # max_sleep_seconds.
-      sleep_seconds = [base_sleep_seconds * (2**(attempts - 1)), max_sleep_seconds].min
-      # Randomize to a random value in the range sleep_seconds/2 .. sleep_seconds
-      sleep_seconds *= (0.5 * (1 + rand))
-      # But never sleep less than base_sleep_seconds
-      sleep_seconds = [base_sleep_seconds, sleep_seconds].max
-      sleep sleep_seconds
+
+      handler && handler.call(exception, attempts)
+
+      sleep delay
       retry
     end
   end
--- test/retries_test.rb
+++ test/retries_test.rb
@@ -2,7 +2,7 @@ require "minitest/autorun"
 require "rr"
 require "timeout"
 
-$:.unshift File.join(File.dirname(__FILE__), "../lib")
+$LOAD_PATH.unshift File.join(File.dirname(__FILE__), "../lib")
 require "retries"
 
 class CustomErrorA < RuntimeError; end
@@ -11,21 +11,21 @@ class CustomErrorB < RuntimeError; end
 class RetriesTest < Minitest::Test
   def test_retries_until_successful
     tries = 0
-    result = with_retries(max_tries: 4, base_sleep_seconds: 0, max_sleep_seconds: 0,
-                          recover: CustomErrorA) do |attempt|
+
+    result = with_retries(max_tries: 4, recover: CustomErrorA) do |attempt|
       tries += 1
-      # Verify that the attempt number passed in is accurate
       assert_equal tries, attempt
       raise CustomErrorA.new if tries < 4
       "done"
     end
+
     assert_equal "done", result
     assert_equal 4, tries
   end
 
   def test_re_raises_after_max_tries
     assert_raises(CustomErrorA) do
-      with_retries(base_sleep_seconds: 0, max_sleep_seconds: 0, recover: CustomErrorA) do
+      with_retries(recover: CustomErrorA) do
         raise CustomErrorA.new
       end
     end
@@ -33,50 +33,54 @@ class RetriesTest < Minitest::Test
 
   def test_rescue_standarderror_if_no_rescue_is_specified
     tries = 0
-    with_retries(base_sleep_seconds: 0, max_sleep_seconds: 0) do
+
+    with_retries do
       tries += 1
-      if tries < 2
-        raise CustomErrorA, "boom"
-      end
+      raise CustomErrorA, "boom" if tries < 2
     end
+
     assert_equal 2, tries
   end
 
   def test_immediately_raise_any_exception_not_specified_by_rescue
     tries = 0
+
     assert_raises(CustomErrorA) do
-      with_retries(base_sleep_seconds: 0, max_sleep_seconds: 0, recover: CustomErrorB) do
+      with_retries(recover: CustomErrorB) do
         tries += 1
         raise CustomErrorA.new
       end
     end
+
     assert_equal 1, tries
   end
 
   def test_allow_for_catching_any_of_multiple_exceptions_specified_by_rescue
-    result = with_retries(max_tries: 3, base_sleep_seconds: 0, max_sleep_seconds: 0,
+    result = with_retries(max_tries: 3,
                           recover: [CustomErrorA, CustomErrorB]) do |attempt|
       raise CustomErrorA.new if attempt == 0
       raise CustomErrorB.new if attempt == 1
       "done"
     end
+
     assert_equal "done", result
   end
 
   def test_run_handler_with_the_expected_args_upon_each_handled_exception
     exception_handler_run_times = 0
     tries = 0
-    handler = Proc.new do |exception, attempt_number|
+
+    handler = proc do |exception, attempt_number|
       exception_handler_run_times += 1
-      # Check that the handler is passed the proper exception and attempt number
       assert_equal exception_handler_run_times, attempt_number
       assert exception.is_a?(CustomErrorA)
     end
-    with_retries(max_tries: 4, base_sleep_seconds: 0, max_sleep_seconds: 0,
-                 handler: handler, recover: CustomErrorA) do
+
+    with_retries(max_tries: 4, handler: handler, recover: CustomErrorA) do
       tries += 1
       raise CustomErrorA.new if tries < 4
     end
+
     assert_equal 4, tries
     assert_equal 3, exception_handler_run_times
   end
